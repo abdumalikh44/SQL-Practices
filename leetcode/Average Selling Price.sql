cc8736490ece034eb59e4b6518277acb9b963697
Abdu Malikh Silaban <106543950+abdumalikh44@users.noreply.github.com>
2026-04-23 00:05:41 +0700

    Fix SQL query formatting and case consistency

--- leetcode/Average Selling Price.sql
+++ leetcode/Average Selling Price.sql
@@ -1,9 +1,9 @@
 # Write your MySQL query statement below
-SELECT 
+select
     a.product_id,
     ROUND(IFNULL(SUM(b.units * a.price) / SUM(b.units), 0), 2) AS average_price
-FROM Prices a
-LEFT JOIN UnitsSold b
-    ON a.product_id = b.product_id
-    AND b.purchase_date BETWEEN a.start_date AND a.end_date
-GROUP BY a.product_id
+from Prices a
+left join UnitsSold b
+    on a.product_id = b.product_id
+    and b.purchase_date between a.start_date and a.end_date
+group by a.product_id
