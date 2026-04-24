706e94ba1f918df5eab123609fa0bcb6fdbde2f9
Abdu Malikh Silaban <106543950+abdumalikh44@users.noreply.github.com>
2026-04-24 22:57:45 +0700

    Refactor SQL query to calculate quality and percentage

--- leetcode/Queries Quality and Percentage.sql
+++ leetcode/Queries Quality and Percentage.sql
@@ -1,23 +1,6 @@
-# Write your MySQL query statement below
-with dog as (
- select
-  query_name,
-  round(sum(rating / position), 2) as quality,
-  round(1 / count(query_name) * 100, 2) as poor_query_percentage
- from queries
- where query_name = "dog"
- group by 1
-)
-
-, cat as(
 select
-  query_name,
-  round(sum(rating / position), 2) as quality,
-  round(1 / count(query_name) * 100, 2) as poor_query_percentage
- from queries
- where query_name = "cat"
-)
-
-select * from dog
-union all 
-select * from cat
+ query_name,
+ round(avg(rating / position), 2) as quality,
+ round(avg(rating < 3) * 100, 2) as poor_query_percentage
+from queries
+group by 1
