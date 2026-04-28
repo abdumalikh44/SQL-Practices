00aab3fc35c7ee2770b1c9a65e4592000cf0fc00
Abdu Malikh Silaban <106543950+abdumalikh44@users.noreply.github.com>
2026-04-28 23:34:43 +0700

    Delete User Activity for the Past 30 Days I.sql

--- User Activity for the Past 30 Days I.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-select
- activity_date as day,
- count(user_id) as active_users
-from activity
-where (user_id, activity_date) IN (select user_id, count(activity_date) from activity group by user_id)
