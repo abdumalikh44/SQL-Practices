695847a07911bd2e7bc4237e6d1b5515d02dac0f
Abdu Malikh Silaban <106543950+abdumalikh44@users.noreply.github.com>
2026-04-29 21:00:12 +0700

    Delete leetcode/Product Sales Analysis III.sql

--- leetcode/Product Sales Analysis III.sql
+++
@@ -1,8 +0,0 @@
-# Write your MySQL query statement below
-select 
- product_id,
- year as first_year,
- quantity,
- price
-from sales
-where (product_id, year) IN (select product_id, MIN(year) from sales group by product_id) 
